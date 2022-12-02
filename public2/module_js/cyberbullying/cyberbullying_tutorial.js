df6125299f3510b364410b7f2937c5ab96b179bd
Joss Tripoli <jct324@lehigh.edu>
2022-12-01 19:22:53 -0500

    finished first module

--- public2/module_js/cyberbullying/cyberbullying_tutorial.js
+++ public2/module_js/cyberbullying/cyberbullying_tutorial.js
@@ -30,22 +30,21 @@ const stepsList = [
     audioFile: ['CUSML.6.3.2.mp3']
   },
   {
-    element: '#nextPage',
-
+    element: '#harmony-page',
     intro: `A <b>troll</b> is a fake social media account, often created to spread misleading information.`,
     position: 'right',
     scrollTo: 'tooltip',
     audioFile: ['CUSML.6.3.3.mp3']
   },
   {
-    element: '#nextPage',
+    element: '#harmony-page',
     intro: `Harmony is an example of a <b>troll</b> we attributed to the Russian Internet Research Agency in March 2020 and worked with Twitter to have her suspended.`,
     position: 'right',
     scrollTo: 'tooltip',
     audioFile: ['CUSML.6.3.3.mp3']
   },
   {
-    element: '#populated-data',
+    element: '.bio',
     intro: `The troll’s account is <b>populated</b> with data that makes them look believable and fit the <b>narrative</b> that the malicious actor wants to push (e.g. a patriotic college girl)`,
     position: 'right',
     scrollTo: 'tooltip',             
@@ -54,29 +53,37 @@ const stepsList = [
   {
     element: '#harmony-pic',
     intro: `Like many other trolls, Harmony poses as a <b>young woman</b>. This is a common tactic of internet charlatans; trolls and bots sell disinformation in the same way advertisers sell their products`,
-    position: 'right',
+    position: 'top',
     scrollTo: 'tooltip',
     audioFile: ['CUSML.6.3.5.mp3']
   },
   {
-    element: '#tweets-number',
+    element: '#twitterNav',
     intro: `Notice harmony's profile statistics. She has a lot of tweets for only being on Twitter since November! <b>Bots</b> are made to post regularly follow and like eachother's content.`,
-    position: 'left',
+    position: 'right',
     scrollTo: 'tooltip',
     audioFile: ['CUSML.6.3.6.mp3']
   },
   {
-    element: '#opinions',
+    element: '#opinion',
     intro: `She has very <b>strong political opinions</b> and sets herself up in direct <b>opposition</b> to an extreme version of people on the other side of the political divide. Harmony’s goal is to make us more disgusted with one another and make meaningful compromise more difficult.`,
-    position: 'left',
+    position: 'right',
     scrollTo: 'tooltip',
     audioFile: ['CUSML.6.3.7.mp3']
   },
   {
-    element: '#reactions',
+    element: '#actionsOverlay',
     intro: `Unwitting legitimate accounts react to the content posted by the troll accounts, e.g., re-sharing it or interacting directly with them. This will turn the <b>disinformation seeds planted</b> by the malicious actor into an <b>organic disinformation campaign</b> where content is shared by both troll accounts and legitimate users`,
-    position: 'left',
+    position: 'right',
+    scrollTo: 'tooltip',
+    audioFile: ['CUSML.6.3.6.mp3']
+  },
+  {
+    element: '#decline',
+    intro: `This is a troll, you should decline their request`,
+    position: 'right',
     scrollTo: 'tooltip',
     audioFile: ['CUSML.6.3.6.mp3']
   }
 ];
+
